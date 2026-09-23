d2a9d8163a17a691f4e3a98ce7d6a351bca0753b
Facundo <facundohw22@gmail.com>
2026-09-23 18:12:18 -0300

    nueva carpeta

--- javascript/js/utils.js
+++ javascript/js/utils.js
@@ -9,19 +9,3 @@ export function estaAprobado(notas,minima) {
     }
     return respuesta;
 }
-
-class Estudiante {
-    constructor(nombre, notas) {
-        this.nombre = nombre;
-        this.notas  = notas;
-    }
-    getNombre(){ return this.nombre; }
-    getPromedio() { return calcularPromedio(this.notas); }
-}
-
-Estudiante, { calcularPromedio};
-
-const facu = new Estudiante('Facundo', [10, 6, 7]);
-const jorge = new Estudiante('Jorge', [5, 5, 7]);
-console.log([facu.getNombre(),facu.getPromedio(),estaAprobado(facu.getPromedio(),6)]);
-console.log([jorge.getNombre(),jorge.getPromedio(),estaAprobado(jorge.getPromedio(),6)]);
